3ce07f6077ed04586307c89810247e2d4f8d22fc
niharikaagg <124886492+niharikaagg@users.noreply.github.com>
2024-08-04 14:42:46 +0530

    Create Largest-Element-in-Array.cpp

---
+++ Largest-Element-in-Array.cpp
@@ -0,0 +1,54 @@
+// https://www.geeksforgeeks.org/problems/largest-element-in-array4009/0?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=largest-element-in-array
+
+/*
+Given an array arr, the task is to find the largest element in it.
+*/
+
+
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+public:
+    int largest(vector<int> &arr, int n)
+    {
+        int max = INT_MIN;
+        
+        for(int i=0; i<n; i++)
+        {
+            if(arr[i] > max)
+                max = arr[i];
+        }
+        return max;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int>arr(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        Solution ob;
+        cout << ob.largest(arr, n) << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
