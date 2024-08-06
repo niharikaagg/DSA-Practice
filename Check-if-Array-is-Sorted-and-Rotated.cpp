be7ce86ad5aeca6f1211abcc875ebeb1b8a409c0
niharikaagg <124886492+niharikaagg@users.noreply.github.com>
2024-08-06 14:44:34 +0530

    Update Check-if-Array-is-Sorted-and-Rotated.cpp

--- Check-if-Array-is-Sorted-and-Rotated.cpp
+++ Check-if-Array-is-Sorted-and-Rotated.cpp
@@ -13,48 +13,64 @@ Note: An array A rotated by x positions results in an array B of the same length
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        int min = INT_MAX;
-        int pos = -1;
 
-        for(int i=0; i<nums.size(); i++)
-        {
-            if(nums[i]<min)
-            {
-                min = nums[i];
-                pos = i;
-            }
-        }
-
-        int i=nums.size()-1;
-        if(pos==0)
-        {
-            while(nums[i]==min)
-            {
-                i--;
-                if(i==-1)
-                    break;
-            }
-        }
-
-        if(i!=nums.size()-1)
-            pos=i+1;
-
-        int next;
+        int count=0;
         for(int i=0; i<nums.size()-1; i++)
         {
-            if(pos==nums.size()-1)
-                next=0;
-            else
-                next = pos+1;
-
-            if(nums[pos]>nums[next])
-                return false;
-
-            if(pos==nums.size()-1)
-                pos=0;
-            else
-                pos++;
+            if(nums[i]>nums[i+1])
+                count++;
         }
-        return true;
+        if(nums[nums.size()-1]>nums[0])
+            count++;
+
+        if(count<=1)
+            return true;
+        return false;
+
+
+
+        // int min = INT_MAX;
+        // int pos = -1;
+
+        // for(int i=0; i<nums.size(); i++)
+        // {
+        //     if(nums[i]<min)
+        //     {
+        //         min = nums[i];
+        //         pos = i;
+        //     }
+        // }
+
+        // int i=nums.size()-1;
+        // if(pos==0)
+        // {
+        //     while(nums[i]==min)
+        //     {
+        //         i--;
+        //         if(i==-1)
+        //             break;
+        //     }
+        // }
+
+        // if(i!=nums.size()-1)
+        //     pos=i+1;
+
+        // int next;
+        // for(int i=0; i<nums.size()-1; i++)
+        // {
+        //     if(pos==nums.size()-1)
+        //         next=0;
+        //     else
+        //         next = pos+1;
+
+        //     if(nums[pos]>nums[next])
+        //         return false;
+
+        //     if(pos==nums.size()-1)
+        //         pos=0;
+        //     else
+        //         pos++;
+        // }
+        // return true;
     }
 };
