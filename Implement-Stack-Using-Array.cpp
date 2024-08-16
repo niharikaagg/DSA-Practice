a08f9d2e5044dfc0429016c7eb57087ab67897f5
niharikaagg <124886492+niharikaagg@users.noreply.github.com>
2024-08-16 20:42:12 +0530

    Create Implement-Stack-Using-Array.cpp

---
+++ Implement-Stack-Using-Array.cpp
@@ -0,0 +1,41 @@
+// https://www.geeksforgeeks.org/problems/implement-stack-using-array/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=implement-stack-using-array
+
+/*
+Write a program to implement a Stack using Array. Your task is to use the class as shown in the comments in the code editor and complete the functions push() and pop() to implement a stack. The push() method takes one argument, an integer 'x' to be pushed into the stack and pop() which returns an integer present at the top and popped out from the stack. If the stack is empty then return -1 from the pop() method.
+
+Note: The input is given in form of queries. Since there are two operations push() and pop(), there is two types of queries as described below:
+(i) 1 x   (a query of this type means  pushing 'x' into the stack)
+(ii) 2     (a query of this type means to pop an element from the stack and print the popped element)
+Input contains separated by space and as described above. 
+*/
+
+
+
+/*
+class MyStack
+{
+private:
+    int arr[1000];
+    int top;
+public:
+    MyStack(){top=-1;}
+    int pop();
+    void push(int);
+};
+*/
+
+void MyStack ::push(int x) {
+    top++;
+    arr[top] = x;
+}
+
+// Function to remove an item from top of the stack.
+int MyStack ::pop() {
+    if(top==-1)
+        return -1;
+    
+    int temp = arr[top];
+    top--;
+    
+    return temp;
+}
