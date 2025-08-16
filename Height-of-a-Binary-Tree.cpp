959a045565aea7a36740a631b1f09868706ba47e
niharikaagg <124886492+niharikaagg@users.noreply.github.com>
2025-08-17 01:06:04 +0530

    Create Height-of-a-Binary-Tree.cpp

---
+++ Height-of-a-Binary-Tree.cpp
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/
+
+/*
+Given the root of a binary tree, return its maximum depth.
+
+A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+*/
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root == NULL)
+            return 0;
+
+        int lh = maxDepth(root->left);
+        int rh = maxDepth(root->right);
+
+        // add your own depth and return maximum from both sides
+        return 1 + max(lh, rh);
+    }
+};
