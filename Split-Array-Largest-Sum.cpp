3d58f3560c9247c73a18377b252f740f4066e0e1
niharikaagg <124886492+niharikaagg@users.noreply.github.com>
2025-08-26 00:56:59 +0530

    Create Split-Array-Largest-Sum.cpp

---
+++ Split-Array-Largest-Sum.cpp
@@ -0,0 +1,52 @@
+// https://leetcode.com/problems/split-array-largest-sum/
+
+/*
+Given an integer array nums and an integer k, split nums into k non-empty subarrays such that the largest sum of any subarray is minimized.
+
+Return the minimized largest sum of the split.
+
+A subarray is a contiguous part of the array.
+*/
+
+
+
+class Solution {
+public:
+    int numberOfSubarrays(vector<int> &nums, int n, int k, int maxSum)
+    {
+        int subarrays = 1, sum = 0;
+
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(sum + nums[i] > maxSum)
+            {
+                subarrays++;
+                sum = nums[i];
+            }
+
+            else
+                sum += nums[i];
+        }
+
+        return subarrays;
+    }
+
+    int splitArray(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        int s = *max_element(nums.begin(), nums.end());
+        int e = accumulate(nums.begin(), nums.end(), 0);
+
+        while(s <= e)
+        {
+            int mid = s + (e-s)/2;
+
+            if(numberOfSubarrays(nums, n, k, mid) <= k)
+                e = mid - 1;
+            else
+                s = mid + 1;
+        }
+
+        return s;
+    }
+};
