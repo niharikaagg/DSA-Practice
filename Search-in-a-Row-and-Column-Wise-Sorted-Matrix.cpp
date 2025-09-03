9b9cac0a6419446a3b6db03717ffa64793f9d2b9
niharikaagg <124886492+niharikaagg@users.noreply.github.com>
2025-09-04 02:58:41 +0530

    Create Search-in-a-Row-and-Column-Wise-Sorted-Matrix.cpp

---
+++ Search-in-a-Row-and-Column-Wise-Sorted-Matrix.cpp
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/search-a-2d-matrix-ii/
+
+/*
+Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
+
+Integers in each row are sorted in ascending from left to right.
+Integers in each column are sorted in ascending from top to bottom.
+*/
+
+
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        int row = 0, col = m - 1;
+
+        while(row<n && col>=0)
+        {
+            if(matrix[row][col] == target)
+                return true;
+            else if(matrix[row][col] > target)
+                col--;
+            else
+                row++;
+        }
+
+        return false;
+    }
+};
