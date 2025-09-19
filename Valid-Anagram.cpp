29bcf068d723cc747dd5e1fc9c29e0cbd73bdd54
niharikaagg <124886492+niharikaagg@users.noreply.github.com>
2025-09-20 00:16:22 +0530

    Update Valid-Anagram.cpp

--- Valid-Anagram.cpp
+++ Valid-Anagram.cpp
@@ -8,6 +8,32 @@ An Anagram is a word or phrase formed by rearranging the letters of a different 
 
 
 
+// with map
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length())
+            return false;
+
+        unordered_map<char, int> mp;
+
+        for(int i=0; i<s.length(); i++)
+            mp[s[i]]++;
+
+        for(int i=0; i<t.length(); i++)
+        {
+            if(mp.find(t[i]) == mp.end() || mp[t[i]] == 0)
+                return false;
+            mp[t[i]]--;
+        }
+
+        return true;
+    }
+};
+
+// without map
+
 class Solution {
 public:
     bool isAnagram(string s, string t) {
