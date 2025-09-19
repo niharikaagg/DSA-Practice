ddd02621dbc721b9aeebae9a8efa641e2be58242
niharikaagg <124886492+niharikaagg@users.noreply.github.com>
2025-09-19 23:38:48 +0530

    Update Two-Sum.cpp

--- Two-Sum.cpp
+++ Two-Sum.cpp
@@ -10,6 +10,28 @@ You can return the answer in any order.
 
 
 
+// Optimized - with hashmap
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+
+        for(int i=0; i<nums.size(); i++)
+        {
+            int reqd = target - nums[i];
+
+            if(mp.find(reqd) != mp.end())
+                return {mp[reqd], i};
+            mp[nums[i]] = i;
+        }
+
+        return {-1,-1};
+    }
+};
+
+// Without hashmap
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
