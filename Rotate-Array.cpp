1329bc58a092e69f051460e8d4f6664dce9c0971
niharikaagg <124886492+niharikaagg@users.noreply.github.com>
2025-09-22 18:33:55 +0530

    Update Rotate-Array.cpp

--- Rotate-Array.cpp
+++ Rotate-Array.cpp
@@ -5,6 +5,21 @@ Given an integer array nums, rotate the array to the right by k steps, where k i
 */
 
 
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        
+        if(k > nums.size())
+            k = k%nums.size();
+
+        reverse(nums.begin(), nums.end() - k);
+        reverse(nums.end() - k, nums.end());
+        reverse(nums.begin(), nums.end());
+    }
+};
+
+// Manual reverse
 class Solution {
 public:
 
