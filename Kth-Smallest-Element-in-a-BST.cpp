1dfdafd3eb78a2eb1cddd0e342eff759045d3e27
niharikaagg <124886492+niharikaagg@users.noreply.github.com>
2025-09-24 00:36:22 +0530

    Create Kth-Smallest-Element-in-a-BST.cpp

---
+++ Kth-Smallest-Element-in-a-BST.cpp
@@ -0,0 +1,45 @@
+// https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+
+/*
+Given the root of a binary search tree, and an integer k, return the kth smallest value (1-indexed) of all the values of the nodes in the tree.
+*/
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int count = 0, result = -1;
+
+    void inorder(TreeNode* root, int k)
+    {
+        if(root == NULL)
+            return;
+
+        inorder(root->left, k);
+
+        count++;
+        if(count == k)
+        {
+            result = root->val;
+            return;
+        }
+
+        inorder(root->right, k);
+    }
+
+    int kthSmallest(TreeNode* root, int k) {
+        inorder(root, k);
+        return result;
+    }
+};
