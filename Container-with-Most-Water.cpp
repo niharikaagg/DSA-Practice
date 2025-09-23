292783c275a4e29dea075ea91e87f5214853789a
niharikaagg <124886492+niharikaagg@users.noreply.github.com>
2025-09-23 13:03:40 +0530

    Create Container-with-Most-Water.cpp

---
+++ Container-with-Most-Water.cpp
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/container-with-most-water/
+
+/*
+You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+Return the maximum amount of water a container can store.
+
+Notice that you may not slant the container.
+*/
+
+
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int l = 0, r = height.size()-1;
+        int area=0, maxArea=0;
+
+        while(l < r)
+        {
+            area = min(height[l], height[r]) * (r-l);
+            maxArea = max(maxArea, area);
+
+            if(height[l] <= height[r])
+                l++;
+            else
+                r--;
+        }
+
+        return maxArea;
+    }
+};
