02aa072e674558e952d35f73ce86b87fd4b0d377
niharikaagg <124886492+niharikaagg@users.noreply.github.com>
2025-10-02 01:22:30 +0530

    Create Distinct-Subsequences.cpp

---
+++ Distinct-Subsequences.cpp
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/distinct-subsequences/
+
+/*
+Given two strings s and t, return the number of distinct subsequences of s which equals t.
+
+The test cases are generated so that the answer fits on a 32-bit signed integer.
+*/
+
+
+
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int m = s.length();
+        int n = t.length();
+
+        vector<vector<double>> dp(m+1, vector<double>(n+1, 0));
+
+        for(int i=0; i<=m; i++)
+            dp[i][0] = 1;
+
+        for(int i=1; i<=m; i++)
+        {
+            for(int j=1; j<=n; j++)
+            {
+                if(s[i-1] == t[j-1])
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                else
+                    dp[i][j] = dp[i-1][j];
+            }
+        }
+
+        return (int)dp[m][n];
+    }
+};
